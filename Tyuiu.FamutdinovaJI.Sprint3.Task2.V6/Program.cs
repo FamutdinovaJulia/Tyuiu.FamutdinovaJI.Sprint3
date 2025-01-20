f02fe2eb7fce78a05f62aa3a2b3bab8cca41456a
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-20 23:20:28 +0500

    task2

---
+++ Tyuiu.FamutdinovaJI.Sprint3.Task2.V6/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint3.Task2.V6.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint3.Task2.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("* Переменная Х = " + value);
+            Console.WriteLine("* Старт шага = " + startValue);
+            Console.WriteLine("* Конец шага = " + stopValue);
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
