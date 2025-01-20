31fab039ceaefac8b6e85269f6146ee4e4cf9a51
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-21 03:54:13 +0500

    task4

---
+++ Tyuiu.FamutdinovaJI.Sprint3.Task4.V6/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint3.Task4.V6.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint3.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("* Старт шага: " + startValue);
+            Console.WriteLine("* Конец шага: " + stopValue);
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
